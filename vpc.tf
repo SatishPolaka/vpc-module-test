4da4c476266fdf16a1e59220c5d2dc89e8877927
SatishPolaka <satishpolaka@gmail.com>
2025-02-05 22:48:58 +0530

    vpc-module-test

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+  source = "../terraform-aws-vpc"
+  project_name = "expense"
+  environment = "dev"
+  vpc_cidr = "10.0.0.0/16"
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+  
+}
